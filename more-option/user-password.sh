59524bc1c93d10a965fe0a312e1c3662c582cd1f
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-27 19:09:23 +0800

    Add files via upload

---
+++ more-option/user-password.sh
@@ -0,0 +1,41 @@
+# Created by NYARIGRATISAN
+#wget https://github.com/muhammadnoor674/
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+echo -e "\e[32mloading...\e[0m"
+clear
+echo " "
+echo " "
+echo " "
+read -p "Input USERNAME to change password: " username
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+read -p "Input new PASSWORD for user $username: " password
+
+clear
+echo "Connecting to Server..."
+sleep 0.5
+echo "Generating New Password..."
+sleep 0.5
+  egrep "^$username" /etc/passwd >/dev/null
+  echo -e "$password\n$password" | passwd $username
+  clear
+  echo " "
+  echo " "
+  echo " "
+  echo "-------------------------------------------"
+  echo -e "Password for user ${blue}$username${NC} successfully changed."
+  echo -e "The new Password for user ${blue}$username${NC} is ${red}$password${NC}"
+  echo "-------------------------------------------"
+  echo " "
+  echo " "
+  echo " "
+
+else
+echo " "
+echo -e "Username ${red}$username${NC} not found in your VPS"
+echo " "
+exit 0
+fi
