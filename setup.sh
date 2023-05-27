ed8480f3682016ac3a71024f14bdd6299b12a55b
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-27 19:41:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,58 +50,12 @@ NC='\e[0m'
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
 clear
-echo -e "\e[32mloading...\e[0m"
-clear
 mkdir /var/lib/premium-script;
-default_email=$( curl https://raw.githubusercontent.com/muhammadnoor674/email/main/default.conf )
-clear
-#Nama penyedia script
-echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
-echo ""
-echo -e "   \e[1;32mPlease enter the name of Provider for Script."
-read -p "   Name : " nm
-echo $nm > /root/provided
-echo ""
-#Email domain
-echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
-echo -e ""
-echo -e "   \e[1;32mPlease enter your email Domain/Cloudflare."
-echo -e "   \e[1;31m(Press ENTER for default email)\e[0m"
-read -p "   Email : " email
-default=${default_email}
-new_email=$email
-# email
-mkdir -p /usr/local/etc/xray/
-touch /usr/local/etc/xray/email
-echo $sts > /usr/local/etc/xray/email
-echo ""
-echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
-echo ""
-echo -e "   .----------------------------------."
-echo -e "   |\e[1;32mPlease select a domain type below \e[0m|"
-echo -e "   '----------------------------------'"
-echo -e "     \e[1;32m1)\e[0m Enter your Subdomain"
-echo -e "     \e[1;32m2)\e[0m Use a random Subdomain"
-echo -e "   ------------------------------------"
-read -p "   Please select numbers 1-2 or Any Button(Random) : " host
-echo ""
-if [[ $host == "1" ]]; then
-echo -e "   \e[1;32mPlease enter your subdomain "
-read -p "   Subdomain: " host1
-echo "IP=" >> /var/lib/premium-script/ipvps.conf
-echo $host1 > /root/domain
-echo ""
-elif [[ $host == "2" ]]; then
+echo "IP=" >> /var/lib/premium-script/ipvps.conf 
 #install cf
 wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
 rm -f /root/cf.sh
 clear
-else
-echo -e "Random Subdomain/Domain is used"
-wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
-rm -f /root/cf.sh
-clear
-fi
 echo ""
 clear
 echo -e "\e[0;32mREADY FOR INSTALLATION SCRIPT...\e[0m"
