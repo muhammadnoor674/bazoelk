3a18dc96275e8d0a7aed33440c0c868c731260c5
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-27 21:09:06 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,36 @@
+#wget https://github.com/muhammadnoor674/
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+echo -e "\e[32mloading...\e[0m"
+clear
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+clear
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+clear
+echo ""
+figlet " SCRIPT" | lolcat
+figlet " BY NYARIGRATISAN" | lolcat
+echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
+echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
+echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
+echo -e "           \e[031;1m> Premium Script By NYARIGRATISAN <\e[0m" | lolcat
+echo -e " \e[0;32m_______________________________________________\e[0m"
+echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
+echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare NYARIGRATISAN         \e[0;32m|\e[0m"
+echo -e "\e[0;32m|               \e[1;35mI HOPE YOU HAPPY                \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;36mTHANKS TO                   \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mALLAH                 \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mMy Wife                  \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mNyariGratisan Family                   \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                    \e[1;35mMy Friends         \e[0;32m|\e[0m"
+echo -e "\e[0;32m|_______________________________________________|\e[0m"
+echo -e "\e[0;32m|_______________\e[36;1mTHANKS For YOUR SUPPORT\e[0m\e[0;32m________________|\e[0m"
+echo -e "               \e[0;32m'----------------'\e[0m"
