2910af0fa15e2f7f4e91a231c7c4551f910b5e0f
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-27 19:47:49 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,16 +35,6 @@ echo -e "$Lyellow                       Tunggu 6 Detik!"$NC
 echo -e "$green.........................................................."$NC
 sleep 6
 clear
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-red='\e[1;31m'
-green='\e[0;32m'
 NC='\e[0m'
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
