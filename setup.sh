27c9508080ba5e534d7d2e2cdbc34f8cc7c36288
Muhammad Noor <60285067+muhammadnoor674@users.noreply.github.com>
2023-05-27 20:25:27 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,168 @@
+#wget https://github.com/muhammadnoor674/
+# Color Validation
+Lred='\e[1;91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+green='\e[32m'
+RED='\033[0;31m'
+NC='\033[0m'
+BGBLUE='\e[1;44m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0;37m'
+# ===================
+echo ''
+clear
+echo ''
+echo -e "$RED         AUTO SCRIPT PREMIUM $NC"
+echo -e "$RED         CREATED BY ANUYBAZOELK $NC"
+echo -e "$RED         WELCOME TO NYARIGRATISAN $NC"
+echo -e "$RED         CONTACT ME wa.me/6285349326511 $NC"
+echo -e "$RED         TELEGRAM @anuybazoelk @anuybazoelk639 $NC"
+echo -e "$RED         DONT FORGET TO FOLLOW MY SNACKVIDEO ACCOUNT $NC"
+echo "                                                              "
+echo -e "$Lyellow                ⚡ PREMIUM SPEED SCRIPT ⚡"$NC
+echo -e "$green.........................................................."$NC
+echo -e "$Lyellow                  Autoscript By NYARIGRATISAN"$NC
+echo -e "$Lyellow                    CONTACT TELEGRAM"$NC
+echo -e "$Lyellow                       @anuybazoelk"$NC
+echo -e "$Lyellow                       @anuybazoelk639"$NC
+echo -e "$green.........................................................."$NC
+echo ''
+echo -e "$Lyellow                       GASPOOL!"$NC
+echo -e "$green.........................................................."$NC
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+if [ -f "/etc/xray/domain" ]; then
+echo "Script Sukses Terinstall"
+exit 0
+fi
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+#install cf
+wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/cf.sh && chmod +x cf.sh && ./cf.sh
+#install ssh ovpn
+echo -e "\e[0;32mINSTALLING SSH & OVPN...\e[0m"
+wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+echo -e "\e[0;32mDONE INSTALLING SSH & OVPN\e[0m"
+clear
+#install Xray
+echo -e "\e[0;32mINSTALLING XRAY CORE...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/ins-xray.sh && chmod +x ins-xray.sh && screen -S ins-xray ./ins-xray.sh
+echo -e "\e[0;32mDONE INSTALLING XRAY CORE\e[0m"
+#install websocket
+echo -e "\e[0;32mINSTALLING WEBSOCKET PORT...\e[0m"
+wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/websocket-python/websocket.sh && chmod +x websocket.sh && screen -S websocket.sh ./websocket.sh
+echo -e "\e[0;32mDONE INSTALLING WEBSOCKET PORT\e[0m"
+#install SET-BR
+echo -e "\e[0;32mINSTALLING SET-BR...\e[0m"
+wget https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+echo -e "\e[0;32mDONE INSTALLING SET-BR...\e[0m"
+# install cloudflare JQ
+apt install jq curl -y
+# install webserver
+apt -y install nginx
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/nginx.conf"
+mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/muhammadnoor674/bazoelk/main/vps.conf"
+/etc/init.d/nginx restart
+#finish
+rm -f /root/ssh-vpn.sh
+rm -f /root/ins-xray.sh
+rm -f /root/ohp.sh
+rm -f /root/ohp-dropbear.sh
+rm -f /root/ohp-ssh.sh
+rm -f /root/websocket.sh
+rm -f /root/set-br.sh
+rm -f /root/cf.sh
+# Colour Default
+echo "1;36m" > /etc/banner
+echo "30m" > /etc/box
+echo "1;31m" > /etc/line
+echo "1;32m" > /etc/text
+echo "1;33m" > /etc/below
+echo "47m" > /etc/back
+echo "1;35m" > /etc/number
+echo 3d > /usr/bin/test
+clear
+echo "Installation has been completed!!"
+echo " "
+echo -e "\e[1;32m══════════════════Autoscript PREMIUM══════════════════\e[0m" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI SSH & OpenVPN]" | tee -a log-install.txt
+echo "    -------------------------" | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200"  | tee -a log-install.txt
+echo "   - OpenVPN SSL             : 110"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 143, 109"  | tee -a log-install.txt
+echo "   - OHP Dropbear            : 8585"  | tee -a log-install.txt
+echo "   - OHP SSH                 : 8686"  | tee -a log-install.txt
+echo "   - OHP OpenVPN             : 8787"  | tee -a log-install.txt
+echo "   - Websocket SSH(HTTP)     : 80"  | tee -a log-install.txt
+echo "   - Websocket SSL(HTTPS)    : 443, 2096"  | tee -a log-install.txt
+echo "   - Websocket OpenVPN       : 2097"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI Sqd, Bdvp, Ngnx]" | tee -a log-install.txt
+echo "    ---------------------------" | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8000 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI XRAY]"  | tee -a log-install.txt
+echo "    ----------------" | tee -a log-install.txt
+echo "   - Xray Vmess Ws Tls       : 443"  | tee -a log-install.txt
+echo "   - Xray Vless Ws Tls       : 443"  | tee -a log-install.txt
+echo "   - Xray Trojan Ws Tls      : 443"  | tee -a log-install.txt
+echo "   - Xray Vless Tcp Xtls     : 443"  | tee -a log-install.txt
+echo "   - Xray Trojan Tcp Tls     : 443"  | tee -a log-install.txt
+echo "   - Xray Vmess Ws None Tls  : 80"  | tee -a log-install.txt
+echo "   - Xray Vless Ws None Tls  : 80"  | tee -a log-install.txt
+echo "   - Xray Trojan Ws None Tls : 80"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI CLASH FOR ANDROID (YAML)]"  | tee -a log-install.txt
+echo "    -----------------------------------" | tee -a log-install.txt
+echo "   - Xray Vmess Ws Yaml      : Yes"  | tee -a log-install.txt
+echo "   - Xray Vless Ws Yaml      : Yes"  | tee -a log-install.txt
+echo "   - Xray Trojan Ws Yaml     : Yes"  | tee -a log-install.txt
+echo "   --------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +8" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo -e "\e[1;32m══════════════════Autoscript By NYARIGRATISAN══════════════════\e[0m" | tee -a log-install.txt
+sleep 7
+clear
+echo ""
+echo -e "    \e[1;32m.------------------------------------------.\e[0m"
+echo -e "    \e[1;32m|     SUCCESFULLY INSTALLED THE SCRIPT     |\e[0m"
+echo -e "    \e[1;32m|         PREMIUM BY @anuybazoelk          |\e[0m"
+echo -e "    \e[1;32m'------------------------------------------'\e[0m"
+echo ""
+echo -e "   \e[1;32mYour VPS Will Be Automatical Reboot In 5 seconds\e[0m"
+rm -r setup.sh
+sleep 5
+reboot
